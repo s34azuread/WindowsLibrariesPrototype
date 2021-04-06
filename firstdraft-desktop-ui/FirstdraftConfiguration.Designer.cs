1f796ac21e27d7850996c42763a51bd9b43618af
firstdraft <81296586+s34azuread@users.noreply.github.com>
2021-04-06 10:28:51 +0530

    Changing message box label

--- firstdraft-desktop-ui/FirstdraftConfiguration.Designer.cs
+++ firstdraft-desktop-ui/FirstdraftConfiguration.Designer.cs
@@ -38,20 +38,20 @@ namespace firstdraft_desktop_ui
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(182, 66);
+            this.button1.Size = new System.Drawing.Size(324, 66);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Heartbeat";
+            this.button1.Text = "Heartbeat Feature";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(230, 13);
+            this.button2.Location = new System.Drawing.Point(689, 13);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(191, 65);
+            this.button2.Size = new System.Drawing.Size(308, 65);
             this.button2.TabIndex = 1;
-            this.button2.Text = "Heartstop";
+            this.button2.Text = "Heartstop Feature";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
